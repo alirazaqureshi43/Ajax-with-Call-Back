f9795af4d295147274a3afe36791b449d5101a4c
Ali Raza <alirazaqureshi43@gmail.com>
2022-07-15 15:39:28 +0500

    ajax with callback

---
+++ index.js
@@ -0,0 +1,42 @@
+const http = new easyHTTP
+
+// http.get('https://jsonplaceholder.typicode.com/posts',
+// function(err, post){
+//     if (err){
+//         console.log(err)
+//     } else{
+//         console.log(post)
+//     }
+// })
+
+// const data = {
+//     title: "Custom",
+//     body: 'hello'
+// }
+
+// http.post('https://jsonplaceholder.typicode.com/posts',data,
+// function(err, post){
+//     if (err){
+//         console.log(err)
+//     } else{
+//         console.log(post)
+//     }
+// })
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1',data,
+// function(err, post){
+//     if (err){
+//         console.log(err)
+//     } else{
+//         console.log(post)
+//     }
+// })
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1',
+function(err, res){
+    if (err){
+        console.log(err)
+    } else{
+        console.log(res)
+    }
+})
